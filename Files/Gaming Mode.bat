f9a32a2cd32c2454961e6dbd2f4a7ed7e9767bbc
Shivansh Goel <shivanshgoel009@gmail.com>
2020-08-18 16:10:11 +0530

    Update Gaming Mode.bat

--- Files/Gaming Mode.bat
+++ Files/Gaming Mode.bat
@@ -1,23 +1,10 @@
 @echo off
-tasklist /FI "IMAGENAME eq steam.exe" 2>NUL | find /I /N "steam.exe">NUL
-if "%ERRORLEVEL%"=="0" goto Process_Found
-tasklist /FI "IMAGENAME eq EpicGamesLauncher.exe" 2>NUL | find /I /N "EpicGamesLauncher.exe">NUL
-if "%ERRORLEVEL%"=="0" goto Process_Found
-:Process_NotFound
 taskkill /IM googledrivesync.exe /T /F
 taskkill /IM OneDrive.exe /T /F
 taskkill /IM Chrome.exe /T /F
 taskkill /IM WhatsApp.exe /T /F
 taskkill /IM AnyDesk.exe /T /F
-start "" "C:\Users\shiva\AppData\Local\Discord\Update.exe"
-start "" "C:\Users\shiva\AppData\Local\Discord\app-0.0.306\Discord.exe"
-goto END
-:Process_Found
-taskkill /IM steam.exe /T /F
-taskkill /IM EpicGamesLauncher.exe /T /F
-start "" "C:\Program Files\Google\Drive\googledrivesync.exe"
-start "" "C:\Users\shiva\AppData\Local\Microsoft\OneDrive\OneDrive.exe"
-timeout 2
-wscript "D:\Program Files (x86)\Gaming-Mode\Files\autokey.vbs"
-:END
+taskkill /IM Zoom.exe /T /F
+cd C:\Users\shiva\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Discord Inc\
+start discord
 Exit
