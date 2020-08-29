7cfcab5e61373889322a89483471271b42eb3ba1
Shivansh Goel <shivanshgoel009@gmail.com>
2020-08-29 19:46:35 +0530

    Update Gaming Mode.bat

--- Files/Gaming Mode.bat
+++ Files/Gaming Mode.bat
@@ -5,6 +5,7 @@ taskkill /IM Chrome.exe /T /F
 taskkill /IM WhatsApp.exe /T /F
 taskkill /IM AnyDesk.exe /T /F
 taskkill /IM Zoom.exe /T /F
-cd C:\Users\shiva\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Discord Inc\
+taskkill /IM Bluestacks.exe /T /F
+cd C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Discord Inc\
 start discord
 Exit
